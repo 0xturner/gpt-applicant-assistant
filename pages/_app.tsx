c8c42009488cb382dea5b0d277553b567044d2dd
Turner <38267425+0xturner@users.noreply.github.com>
2023-03-17 14:53:47 -0700

    Setup db (#2)
    * add supabase and configure client
    
    * add auth and sessions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,21 @@ import type { LayoutProps } from "@vercel/examples-ui/layout";
 import { getLayout } from "@vercel/examples-ui";
 
 import "@vercel/examples-ui/globals.css";
+import { useState } from "react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 function App({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component);
+  const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    // <Layout
-    //   title="ai-chatgpt"
-    //   path="solutions/ai-chatgpt"
-    //   description="ai-chatgpt"
-    // >
-    <>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </SessionContextProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { Layout, Text, Page } from "@vercel/examples-ui";
 import { Chat } from "../components/Chat";
 
 function Home() {
+  const session = useSession();
+  console.log("session: ", session);
+  const supabase = useSupabaseClient();
   return (
     <Page className="flex flex-col gap-12">
-      {/* <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
-        <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
-        </Text>
-      </section> */}
-
       <section className="flex flex-col gap-3">
         <Text variant="h2">GPT Job Application Bot:</Text>
         <div className="lg:w-2/3">

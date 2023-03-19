74e2bf15897e1ddfd77b3b84eb593652c75a8f6b
Turner <turnerschumann@gmail.com>
2023-03-18 17:17:59 -0700

    add jd input

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [resumeInput, setResumeInput] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
   const [storedResume, setStoredResume] = useLocalStorage("resume", "");
 
   const submit = async (resume: string) => {
     setStoredResume(resume);
     window.alert(`Submitted: ${resume}`);
+    window.alert(`Submitted: ${jobDescription}`);
   };
 
   // sync the resume in local storage with the resume input state
@@ -36,7 +38,25 @@ export default function Home() {
               submit(resumeInput);
             }
           }}
-        ></textarea>
+        />
+        <label className="label">
+          <span className="label-text">Your job description</span>
+          <span className="label-text-alt">Alt label</span>
+        </label>
+        <textarea
+          className="textarea-bordered textarea h-24"
+          placeholder="Paste the job description here"
+          value={jobDescription}
+          onChange={(e) => {
+            setJobDescription(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              submit(resumeInput);
+            }
+          }}
+        />
+        <button className="btn-primary btn mt-4">Generate</button>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,7 @@
 // import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-// import { Layout, Text, Page } from "@vercel/examples-ui";
+import Header from "@components/Header";
 import { ReactNode } from "react";
 import Home from "../components/Home";
-
-const Header = () => (
-  <div className="navbar bg-base-100">
-    <div className="flex-1">
-      <a className="btn-ghost btn text-xl normal-case">GPT Assistant</a>
-    </div>
-    <div className="flex-none">
-      <button className="btn-outline btn-ghost btn-square btn">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="inline-block h-5 w-5 stroke-current"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-          ></path>
-        </svg>
-      </button>
-    </div>
-  </div>
-);
 
 const Layout = ({ children }: { children: ReactNode }) => (
   <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">

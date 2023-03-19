a9996568f3ec8de6668196f9061689ca6b9258f8
Turner <38267425+0xturner@users.noreply.github.com>
2023-03-18 18:58:29 -0700

    Redo the UI and add utilities for syncing data to localStorage + add some libraries (#4)
    * prettier stuff
    
    * lint
    
    * prettify
    
    * daisy ui
    
    * resume input with local storage sync
    
    * add jd input
    
    * send generate request
    
    * add clsx

---
+++ components/Home.tsx
@@ -0,0 +1,89 @@
+import useLocalStorage from "hooks/useLocalStorage";
+import { useEffect, useState } from "react";
+
+import { useMutation } from "@tanstack/react-query";
+
+const generateAnswer = async (resume: string, jobDescription: string) => {
+  const response = await fetch("/api/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      resume,
+      jobDescription,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
+export default function Home() {
+  const [resumeInput, setResumeInput] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
+  const [storedResume, setStoredResume] = useLocalStorage("resume", "");
+
+  // const submit = async (resume: string) => {
+  //   setStoredResume(resume);
+  //   window.alert(`Submitted: ${resume}`);
+  //   window.alert(`Submitted: ${jobDescription}`);
+  // };
+
+  const mutation = useMutation({
+    mutationFn: () => generateAnswer(resumeInput, jobDescription),
+  });
+  console.log("mutation: ", mutation.data);
+
+  // sync the resume in local storage with the resume input state
+  useEffect(() => {
+    if (storedResume) {
+      setResumeInput(storedResume);
+    }
+  }, [storedResume]);
+
+  return (
+    <div className="mt-4 rounded-2xl  border-zinc-100 lg:border lg:p-4">
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Your resume</span>
+          <span className="label-text-alt">Alt label</span>
+        </label>
+        <textarea
+          className="textarea-bordered textarea h-24"
+          placeholder="Paste your resume here"
+          value={resumeInput}
+          onChange={(e) => {
+            setResumeInput(e.target.value);
+          }}
+        />
+        <label className="label">
+          <span className="label-text">Your job description</span>
+          <span className="label-text-alt">Alt label</span>
+        </label>
+        <textarea
+          className="textarea-bordered textarea h-24"
+          placeholder="Paste the job description here"
+          value={jobDescription}
+          onChange={(e) => {
+            setJobDescription(e.target.value);
+          }}
+        />
+        <button
+          className="btn-primary btn mt-4"
+          onClick={() => {
+            setStoredResume(resumeInput);
+            mutation.mutate();
+          }}
+        >
+          {mutation.isLoading ? "Loading..." : "Generate"}
+        </button>
+      </div>
+      <div className="mt-8 whitespace-pre-line">
+        {mutation.data?.text && mutation.data?.text}
+      </div>
+    </div>
+  );
+}
--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -4,6 +4,9 @@ import { ipRateLimit } from "@lib/ip-rate-limit";
 import { Configuration, OpenAIApi } from "openai";
 import fetchAdapter from "@vespaiach/axios-fetch-adapter";
 
+const SAMPLE_RESPONSE =
+  "Dear Hiring Manager,\n\nI am excited to apply for the Senior Software Engineer position at Orb Labs. As a product-minded software engineer with experience building and scaling front-end and back-end systems, I am confident that I have the skills and expertise to contribute to your mission of empowering every developer to build accessible and scalable omnichain applications.\n\nIn my previous role as a Senior Software Engineer at Fraction, I helped build the foundational codebase and grew the team for a next-gen finance platform. I also architected the payment operations platform, which included automated money movement, bank integration, reconciliation, reporting, and more. My experience in building full-stack product features in a distributed serverless environment and executing hiring initiatives will be valuable in contributing to your team's success.\n\nI am inspired by Orb Labs' mission to enable every individual to access web3 with no friction. Your values of First Principles Thinking, Ownership, Customer Focus, and Urgency align with my own values and work ethic. I am excited to work with a team that is not bound by conventional thinking and is obsessed with delivering the future as fast as possible while being intentional.\n\nThank you for considering my application. I look forward to the opportunity to discuss my qualifications further.\n\nSincerely,\n[Your Name]";
+
 const ACTIVE = false;
 
 // break the app if the API key is missing
@@ -18,6 +21,7 @@ export const config = {
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 const handler = async (req: NextRequest): Promise<Response> => {
@@ -26,11 +30,14 @@ const handler = async (req: NextRequest): Promise<Response> => {
   if (res.status !== 200) return res;
 
   const body = await req.json();
+  // console.log("body: ", body);
+
+  // TODO parse the body
 
   if (!ACTIVE) {
     return NextResponse.json(
       {
-        text: "dummy response",
+        text: SAMPLE_RESPONSE,
       },
       { headers: res.headers }
     );
@@ -40,17 +47,24 @@ const handler = async (req: NextRequest): Promise<Response> => {
     {
       model: "gpt-3.5-turbo",
       messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        { role: "user", content: "Who won the world series in 2020?" },
+        {
+          role: "system",
+          content: "You are a helpful assistant that helps job applicants.",
+        },
+        {
+          role: "user",
+          content: `Write my cover letter for a job application in a conversational tone. Use my resume and the job description below as reference. \nResume:\n${body.resume}\nJob Description:\n${body.jobDescription}`,
+        },
       ],
       temperature: 0.2,
-      max_tokens: 100,
+      max_tokens: 500,
     },
     {
       adapter: fetchAdapter,
     }
   );
 
+  console.log("completion.data.choices: ", completion.data.choices);
   return NextResponse.json(
     {
       text: completion.data.choices[0].message?.content,
